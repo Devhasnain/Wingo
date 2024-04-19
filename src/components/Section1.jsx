abdaa41a88441a0d212cb30b6b553b654bed3faa
mustafa2922 <mustafaraza6372@gmail.com>
2024-04-20 03:07:47 +0500

    updated box sizes

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -216,24 +216,24 @@ const Section1 = () => {
             })}
           </div>
 
-          <div className="grid grid-cols-2 xl:grid-cols-12 items-center gap-2">
-            <div className="col-span-12 xl:col-span-3">
-              <button className="w-full border rounded-lg text-xl font-bold border-[#ff9901] text-[#ff9901] hover:bg-[#ff9901] hover:text-white transition-all py-2 px-3">
+          <div className="grid grid-cols-12 items-center gap-2">
+            <div className="col-span-3">
+              <button className="w-full border rounded-lg text-sm sm:text-xl font-bold border-[#ff9901] text-[#ff9901] hover:bg-[#ff9901] hover:text-white transition-all px-1 py-2 sm:px-3">
                 Random
               </button>
             </div>
-            <div className="col-span-12 xl:col-span-9 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-2">
+            <div className="col-span-9 grid grid-cols-6 gap-1 sm:gap-2">
               {["X1", "X5", "X10", "X20", "X50", "X100"].map((_, index) => {
                 return (
                   <div
                     onClick={() => {
                       setActiveSize(index);
                     }}
-                    className={`p-2 ${
+                    className={`px-0.5 py-2 text-sm sm:text-lg sm:px-2 sm:py-2 ${
                       activeSize === index
                         ? "bg-[#40ad72] text-white"
                         : " bg-gray-200"
-                    } lg:cols-span-2 col-span-1 rounded-lg cursor-pointer text-center`}
+                    } lg:cols-span-2 col-span-1 rounded-lg  cursor-pointer text-center`}
                     key={index}
                   >
                     <span>{_}</span>
